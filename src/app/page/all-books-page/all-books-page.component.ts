6da0717ab02fb9ade9f940083ae1f27acb958876
MoermansJ <108662909+MoermansJ@users.noreply.github.com>
2023-10-16 21:00:02 +0200

    +filter available books

--- src/app/page/all-books-page/all-books-page.component.ts
+++ src/app/page/all-books-page/all-books-page.component.ts
@@ -12,7 +12,9 @@ import { DataService } from 'src/app/service/data.service';
 export class AllBooksPageComponent implements OnInit, OnDestroy {
   //properties
   public allBooks: Book[] = [];
+  public filteredBooks: Book[] = [];
   public queryDescription: string = '';
+  private showOnlyAvailableBooks: boolean = false;
   private dataServiceSubscription: Subscription = new Subscription();
 
   //constructor
@@ -28,14 +30,15 @@ export class AllBooksPageComponent implements OnInit, OnDestroy {
     //subscribing to dataService
     this.dataServiceSubscription = this.dataService
       .getSearchResults()
-      .subscribe((value) => {
-        this.allBooks = value;
+      .subscribe((allBooks) => {
+        this.allBooks = allBooks;
+        this.filteredBooks = allBooks;
       });
 
     this.dataServiceSubscription = this.dataService
       .getQueryDescription()
-      .subscribe((value) => {
-        this.queryDescription = value;
+      .subscribe((queryDescription) => {
+        this.queryDescription = queryDescription;
       });
   }
 
@@ -48,5 +51,15 @@ export class AllBooksPageComponent implements OnInit, OnDestroy {
     setTimeout(() => {
       this.dataService.setClickedBook(book);
     }, 1);
+  }
+
+  public handleToggleShowOnlyAvailableBooks(): void {
+    this.showOnlyAvailableBooks = !this.showOnlyAvailableBooks;
+
+    if (this.showOnlyAvailableBooks) {
+      this.filteredBooks = this.allBooks.filter((book) => book.available);
+    } else {
+      this.filteredBooks = this.allBooks;
+    }
   }
 }
